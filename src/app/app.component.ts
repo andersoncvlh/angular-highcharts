0ada29b5f7dd7491538b237e65b9e69c4f101dce
Anderson Carvalho <anderson.cvlh@gmail.com>
2018-03-22 08:09:26 -0300

    angular with highcharts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy, OnInit, AfterContentInit } from '@angular/core';
+
+import { chart } from 'highcharts';
+
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'bv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterContentInit, OnDestroy {
+
+  @ViewChild('chartTarget') chartTarget: ElementRef;
+
+  chart: Highcharts.ChartObject;
+
   title = 'bv';
+
+  ngOnInit(): void {
+  }
+
+  ngAfterContentInit() {
+    const options: Highcharts.Options = {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: 'Fruit Consumption'
+      },
+      xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+      }, {
+        name: 'John',
+        data: [5, 7, 3]
+      }]
+    };
+
+    this.chart = chart(this.chartTarget.nativeElement, options);
+  }
+
+  ngOnDestroy() {
+    this.chart = null;
+  }
+
 }
